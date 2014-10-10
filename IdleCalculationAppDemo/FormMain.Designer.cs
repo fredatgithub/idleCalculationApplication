e3b8f1b8d8420230a6749747e1fe0d3b2e940a2f
Freddy Juhel <domaine75@gmail.com>
2014-10-10 20:25:49 +0200

    initial commit

---
+++ IdleCalculationAppDemo/FormMain.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace IdleCalculationAppDemo
+{
+  partial class FormMain
+  {
+    /// <summary>
+    /// Variable nécessaire au concepteur.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Nettoyage des ressources utilisées.
+    /// </summary>
+    /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Code généré par le Concepteur Windows Form
+
+    /// <summary>
+    /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+    /// le contenu de cette méthode avec l'éditeur de code.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.labelIdleTime = new System.Windows.Forms.Label();
+      this.timerIdle = new System.Windows.Forms.Timer(this.components);
+      this.SuspendLayout();
+      // 
+      // labelIdleTime
+      // 
+      this.labelIdleTime.AutoSize = true;
+      this.labelIdleTime.Location = new System.Drawing.Point(24, 40);
+      this.labelIdleTime.Name = "labelIdleTime";
+      this.labelIdleTime.Size = new System.Drawing.Size(0, 17);
+      this.labelIdleTime.TabIndex = 0;
+      // 
+      // timerIdle
+      // 
+      this.timerIdle.Enabled = true;
+      this.timerIdle.Interval = 1000;
+      this.timerIdle.Tick += new System.EventHandler(this.timerIdle_Tick);
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(715, 99);
+      this.Controls.Add(this.labelIdleTime);
+      this.Name = "FormMain";
+      this.Text = "Idle Calculation";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FormMain_KeyPress);
+      this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.FormMain_MouseClick);
+      this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMain_MouseDown);
+      this.MouseEnter += new System.EventHandler(this.FormMain_MouseEnter);
+      this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormMain_MouseMove);
+      this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMain_MouseUp);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label labelIdleTime;
+    private System.Windows.Forms.Timer timerIdle;
+  }
+}
+
---
+++ IdleCalculationAppDemo/FormMain.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace IdleCalculationAppDemo
+{
+  public partial class FormMain : Form
+  {
+    public FormMain()
+    {
+      InitializeComponent();
+    }
+
+    private const string IdleText = "The keyboard or mouse have not been touched in this application since ";
+    private TimeSpan _idleTimeSpan;
+    
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      labelIdleTime.Text = IdleText + TimeToString(new TimeSpan(0, 0, 1));
+    }
+
+    private void timerIdle_Tick(object sender, EventArgs e)
+    {
+      _idleTimeSpan = _idleTimeSpan.Add(new TimeSpan(0, 0, 1));
+      labelIdleTime.Text = IdleText + TimeToString(_idleTimeSpan);
+    }
+
+    private static string TimeToString(TimeSpan timeSpan)
+    {
+      string result = string.Empty;
+      if (timeSpan.Days > 0)
+      {
+        result = timeSpan.Days + " day" + Plural(timeSpan.Days) + " ";
+      }
+
+      if (timeSpan.Hours > 0)
+      {
+        result += timeSpan.Hours + " hour" + Plural(timeSpan.Hours) + " ";
+      }
+
+      if (timeSpan.Minutes > 0)
+      {
+        result += timeSpan.Minutes + " minute" + Plural(timeSpan.Minutes) + " ";
+      }
+
+      if (timeSpan.Seconds > 0)
+      {
+        result += timeSpan.Seconds + " second" + Plural(timeSpan.Seconds) + " ";
+      }
+
+      if (timeSpan.Milliseconds > 0)
+      {
+        result += timeSpan.Milliseconds + " millisecond" + Plural(timeSpan.Milliseconds);
+      }
+
+      return result;
+    }
+
+    private static string Plural(int number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+
+    private void FormMain_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      _idleTimeSpan = new TimeSpan();
+    }
+
+    private void FormMain_MouseMove(object sender, MouseEventArgs e)
+    {
+      _idleTimeSpan = new TimeSpan();
+    }
+
+    private void FormMain_MouseDown(object sender, MouseEventArgs e)
+    {
+      _idleTimeSpan = new TimeSpan();
+    }
+
+    private void FormMain_MouseEnter(object sender, EventArgs e)
+    {
+      _idleTimeSpan = new TimeSpan();
+    }
+
+    private void FormMain_MouseClick(object sender, MouseEventArgs e)
+    {
+      _idleTimeSpan = new TimeSpan();
+    }
+
+    private void FormMain_MouseUp(object sender, MouseEventArgs e)
+    {
+      _idleTimeSpan = new TimeSpan();
+    }
+  }
+}
